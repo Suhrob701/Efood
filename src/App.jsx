29f93be6f14248f4e4f476f888fb6ec99c04456b
unknown <suhrobnurmamatov909@gmail.com>
2025-05-16 10:19:19 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header'
 import Section from './Components/Header/Section'
 import Footer from './Components/Header/Footer'
 
+
 const App = () => {
   return (
     <div>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-
-import react from '@vitejs/plugin-react-swc'
-
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
+// vite.config.js
 export default defineConfig({
+  base: "/Efood/", // GitHub Pages uchun kerak
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss()
   ],
 })
+  
